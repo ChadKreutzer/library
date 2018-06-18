84be0ca095d44b15d1521f810fe3a50f9c558b7b
kanthalion <kanthalion@gmail.com>
2018-06-18 04:59:50 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  info() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ?
+      "I've read this"
+			: 'not read yet'}`;
+  }
+}
